e2c8c811149ef15105b92d7e5c8e2190a4373987
cio <casado.cio@gmail.com>
2021-11-01 09:17:08 -0300

    partial commit

--- db-tpe-web2_create.sql
+++ db-tpe-web2_create.sql
@@ -18,9 +18,19 @@ CREATE TABLE subject (
   FOREIGN KEY (direct_requirement) REFERENCES subject(id)
 );
 
-
 CREATE TABLE user (
+  id int NOT NULL AUTO_INCREMENT,
   email varchar(100)  NOT NULL,
   password varchar(100) NOT NULL,
-  PRIMARY KEY (email)
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE comment{
+  subject_id int NOT NULL,
+  user_id INT NOT NULL,
+  comment varchar(250) NOT NULL,  
+  difficulty int NOT NULL,
+  PRIMARY KEY (subject_id,user_id),
+  FOREIGN KEY (subject_id) REFERENCES subject(id),
+  FOREIGN KEY (user_id) REFERENCES user(id),
+}
