be05749bdb0965f4f184a7afd93452b8ce801f8f
cio <casado.cio@gmail.com>
2021-11-14 14:27:09 -0300

    a new career and more comments were added to the data

--- tables_and_data.sql
+++ tables_and_data.sql
@@ -53,29 +53,43 @@ CREATE TABLE comment (
 
 -- password are cio, ema and test
 INSERT INTO user (email, password, permission) VALUES
-    ('cio@gmail.com', '$2y$10$upi5wgUxXhgbgAW1z0iKbekkqF0Q0496Cm6oIkLKBjcJgGJZZ3/MK', 1),
-    ('ema@gmail.com', '$2y$10$/XesZLm8YNdddV6Oy.vQ3e/jbA2YMe5ZtcUFAf.KgVznoWGqcIxMy', 1),
-    ('usuario@prueba.com', '$2y$10$Yd0Dd0W9c9vbK6tchWIzBeiTi32Cm5AVU9Rr.QwSeAvtSVxZoHh3W', 0);
+  ('cio@gmail.com', '$2y$10$upi5wgUxXhgbgAW1z0iKbekkqF0Q0496Cm6oIkLKBjcJgGJZZ3/MK', 1),
+  ('ema@gmail.com', '$2y$10$/XesZLm8YNdddV6Oy.vQ3e/jbA2YMe5ZtcUFAf.KgVznoWGqcIxMy', 1),
+  ('usuario@prueba.com', '$2y$10$Yd0Dd0W9c9vbK6tchWIzBeiTi32Cm5AVU9Rr.QwSeAvtSVxZoHh3W', 0);
 
 INSERT INTO career (id, name, years, faculty) VALUES
-    (NULL, 'Tecnicatura Universitaria en Desarrollo de Aplicaiones informaticas', 3, 'Facultad de Ciencias Exactas');
+  (NULL, 'Tecnicatura Universitaria en Desarrollo de Aplicaiones informaticas', 3, 'Facultad de Ciencias Exactas'),
+  (NULL, 'Ingenieria de Sistemas', 5, 'Facultad de Ciencias Exactas');
 
-INSERT INTO subject (id, semester, year, name, direct_requirement, career, image_path) VALUES
-    (NULL, 1, 1, 'Programación 1', NULL, 1, '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 1, 1, 'Web 1', NULL, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 1, 1, 'Taller de Matemática Compuracional ', NULL, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 1, 1, 'Inglés 1', NULL, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 2, 1, 'Tecnología de la Información en las Organizaciones', NULL, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 2, 1, 'Web 2', 2, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 2, 1, 'Programación 2', 1, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 2, 1, 'Inglés 2', 4, 1,  '/assets/images/subjects/pandora.jpeg'),
-    (NULL, 2, 1, 'Seminario Tecnológico 1', NULL, 1,  '/assets/images/subjects/pandora.jpeg');
+INSERT INTO subject (semester, year, name, direct_requirement, career, image_path) VALUES
+  (1, 1, 'Programación 1', NULL, 1, 'assets/images/subjects/pandora.jpeg'),
+  (1, 1, 'Web 1', NULL, 1,  'assets/images/subjects/pandora.jpeg'),
+  (1, 1, 'Taller de Matemática Compuracional ', NULL, 1,  'assets/images/subjects/pandora.jpeg'),
+  (1, 1, 'Inglés 1', NULL, 1,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Tecnología de la Información en las Organizaciones', NULL, 1, NULL),
+  (2, 1, 'Web 2', 2, 1,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Programación 2', 1, 1,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Inglés 2', 4, 1,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Seminario TecnolÓgico 1', NULL, 1,  'assets/images/subjects/pandora.jpeg'),
+  (1, 1, 'Inglés', NULL, 2,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Análisis y diseño de algoritmos I', NULL, 2,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Análisis y diseño de algoritmos II', NULL, 2, NULL),
+  (2, 1, 'Introducción a la Programación I', NULL, 2,  'assets/images/subjects/pandora.jpeg'),
+  (2, 1, 'Física General', NULL, 2,  NULL),
+  (2, 1, 'Álgebra Lineal', NULL, 2,  'assets/images/subjects/pandora.jpeg');
  
 INSERT INTO comment (subject_id, user_id,comment,difficulty) VALUES
-(1,1,'Muy buena',4),
-(2,2,'buena materia',5),
-(3,1,'buenarda',4),
-(4,2,'mala',5),
-(5,1,'Malisimaa',1),
-(6,2,'media media',2),
-(7,1,'sin comentarios',3);
+  (1,1,'Muy buena',4),
+  (2,2,'buena materia',5),
+  (3,1,'buenarda',4),
+  (4,2,'mala',5),
+  (5,1,'Malisimaa',1),
+  (6,2,'media media',2),
+  (7,1,'sin comentarios',3),
+  (8,1,'Muy buena',4),
+  (9,2,'buena materia',5),
+  (10,1,'buenarda',4),
+  (10,2,'mala',5),
+  (11,1,'Malisimaa',1),
+  (11,2,'media media',2),
+  (12,1,'sin comentarios',3);
